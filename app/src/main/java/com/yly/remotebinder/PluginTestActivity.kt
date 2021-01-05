74cff50af232a819804e68ee83796215399096ea
yuliyang <645644717@qq.com>
2021-01-05 16:14:20 +0800

    修改项目依赖

--- app/src/main/java/com/yly/remotebinder/PluginTestActivity.kt
+++ app/src/main/java/com/yly/remotebinder/PluginTestActivity.kt
@@ -12,13 +12,24 @@ class PluginTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_plugin)
+
+        val apkName = "plugin-debug.apk"
+
+        copyPlugin.setOnClickListener {
+            val inputStream = assets.open(apkName)
+            val outputStream = File(filesDir, apkName).outputStream()
+            inputStream.copyTo(outputStream)
+            inputStream.close()
+            outputStream.close()
+        }
+
         testPlugin.setOnClickListener {
             val dexOutputDir = getDir("dex", 0);
-            val libPath = filesDir.absolutePath + File.separator + "plugin-debug.apk";
+            val libPath = filesDir.absolutePath + File.separator + apkName;
             val loader = DexClassLoader(
                 libPath,
-                dexOutputDir.getAbsolutePath(),
-                null,
+                dexOutputDir.absolutePath,
+                applicationInfo.nativeLibraryDir,
                 classLoader
             )
             val clazz = loader.loadClass("com.yly.plugin.PluginImpl")
